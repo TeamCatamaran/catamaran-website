5e2835e5a33f29b45188b834558811ac710b6117
Phil Hess <phess@andculture.com>
2020-02-21 09:00:10 -0500

    Footer updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import logo from '../img/catamaran-logo.svg'
-import facebook from '../img/social/facebook.svg'
-import instagram from '../img/social/instagram.svg'
-import twitter from '../img/social/twitter.svg'
-import linkedin from '../img/social/vimeo.svg'
+import logo from '../img/footer-logo.png'
+import facebook from '../img/social/facebook.png'
+import instagram from '../img/social/instagram.png'
+import twitter from '../img/social/twitter.png'
+import linkedin from '../img/social/linkedin.png'
 
 const Footer = class extends React.Component {
   render() {
@@ -14,7 +14,7 @@ const Footer = class extends React.Component {
         <div className="c-footer__row">
           <div className="c-footer__section">
             <section>
-              <ul className="menu-list">
+              <ul className="c-footer__menu -primary">
                 <li>
                   <Link className="navbar-item" to="/about">
                     About
@@ -45,7 +45,7 @@ const Footer = class extends React.Component {
           </div>
           <div className="c-footer__section">
             <section>
-              <ul className="menu-list">
+              <ul className="c-footer__menu -secondary">
                 <li>
                   <Link className="navbar-item" to="/blog">
                     Ventures
@@ -74,50 +74,58 @@ const Footer = class extends React.Component {
               </ul>
             </section>
           </div>
-          <div className="c-footer__section">
-            Who needs inbox zero?
+          <div className="c-footer__section -newsletter">
+            <label className="-large">
+              Who needs inbox zero?
+            </label>
           </div>
         </div>
         <div className="c-footer__row">
           <div className="c-footer__section">
             <img
               src={logo}
-              alt="Kaldi"
-              style={{ width: '14em', height: '10em' }}
+              alt="Catamaran"
+              style={{ width: '170px' }}
             />
           </div>
-          <div className="c-footer__section">
-              <a title="twitter" href="https://twitter.com">
+          <div className="c-footer__section -social">
+              <a className="c-footer__social" title="twitter" href="https://twitter.com">
                 <img
                   className="fas fa-lg"
                   src={twitter}
                   alt="Twitter"
-                  style={{ width: '1em', height: '1em' }}
+                  style={{ maxWidth: '1em', maxHeight: '1em' }}
                 />
               </a>
-              <a title="facebook" href="https://facebook.com">
+              <a className="c-footer__social" title="facebook" href="https://facebook.com">
                 <img
                   src={facebook}
                   alt="Facebook"
-                  style={{ width: '1em', height: '1em' }}
+                  style={{ maxWidth: '1em', maxHeight: '1em' }}
                 />
               </a>
-              <a title="instagram" href="https://instagram.com">
+              <a className="c-footer__social" title="instagram" href="https://instagram.com">
                 <img
                   src={instagram}
                   alt="Instagram"
-                  style={{ width: '1em', height: '1em' }}
+                  style={{ maxWidth: '1em', maxHeight: '1em' }}
                 />
               </a>
-              <a title="linkedin" href="https://www.linkedin.com">
+              <a className="c-footer__social" title="linkedin" href="https://www.linkedin.com">
                 <img
                   src={linkedin}
                   alt="Linkedin"
-                  style={{ width: '1em', height: '1em' }}
+                  style={{ maxWidth: '1em', maxHeight: '1em' }}
                 />
               </a>
             </div>
-            <div className="c-footer__section">
+          <div className="c-footer__section">
+            <p className="c-footer__family">
+              part of the <a title="andculture family" href="https://andculture.com" target="_blank">andculture</a> family
+            </p>
+          </div>
+          <div className="c-footer__section -copyright">
+            <p>&copy; Catamaran. All rights reserved. Partner work and information are copyrighted and the property of respective partners.</p>
           </div>
         </div>
       </footer>
