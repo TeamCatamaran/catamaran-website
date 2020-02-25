39b7a4ac9a249b698ce49f5cd9e09e407cc435e5
Phil Hess <phess@andculture.com>
2020-02-25 12:00:35 -0500

    Header style updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,15 +7,17 @@ const Header = class extends React.Component {
 
     render() {
         return (
-            <header className="c-header" style={{
-                backgroundImage: `url(${
-                    !!this.props.image.childImageSharp ? this.props.image.childImageSharp.fluid.src : this.props.image
-                })`,
-            }}>
-                <div className="container">
-                    <div className="c-header__text">
-                        <h2>{this.props.section}</h2>
-                        <h1>{this.props.heading}</h1>
+            <header className="c-header">
+                <div className="c-header__background" style={{
+                    backgroundImage: `url(${
+                        !!this.props.image.childImageSharp ? this.props.image.childImageSharp.fluid.src : this.props.image
+                    })`,
+                }}>
+                    <div className="container">
+                        <div className="c-header__text">
+                            <h2>{this.props.section}</h2>
+                            <h1>{this.props.heading}</h1>
+                        </div>
                     </div>
                 </div>
             </header>
