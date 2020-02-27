6cafa2a730fd2bdc7dbc1af5e2215152dece23aa
Phil Hess <phess@andculture.com>
2020-02-27 12:06:05 -0500

    update network page preview

--- src/cms/preview-templates/NetworkPagePreview.js
+++ src/cms/preview-templates/NetworkPagePreview.js
@@ -2,20 +2,28 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { NetworkPageTemplate } from '../../templates/network-page'
 
-const NetworkPagePreview = ({ entry, widgetFor }) => (
-  <NetworkPageTemplate
-    heading={data.heading}
-    section={data.section}
-    intro={data.intro}
-    partners={data.partners || []}
-  />
-)
+const NetworkPagePreview = ({ entry, widgetFor }) => {
+  const data = entry.getIn(['data']).toJS()
+
+  if (data) {
+    return (
+        <NetworkPageTemplate
+          heading={data.heading}
+          section={data.section}
+          intro={data.intro}
+          partners={data.partners || []}
+        />
+    )
+  } else {
+    return <div>Loading...</div>
+  }
+}
 
 NetworkPagePreview.propTypes = {
-  entry: PropTypes.shape({
-    getIn: PropTypes.func,
-  }),
-  widgetFor: PropTypes.func,
+    entry: PropTypes.shape({
+        getIn: PropTypes.func,
+    }),
+    widgetFor: PropTypes.func,
 }
 
 export default NetworkPagePreview
