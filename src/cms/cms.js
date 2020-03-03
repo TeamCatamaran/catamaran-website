8c15009ed6f6d23c4060f305b531092d254a05a5
Phil Hess <phess@andculture.com>
2020-03-03 15:15:00 -0500

    Add journal page

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,6 +4,7 @@ import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import ContactPagePreview from './preview-templates/ContactPagePreview'
+import JournalPagePreview from './preview-templates/JournalPagePreview'
 import JournalPostPreview from './preview-templates/JournalPostPreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 import NetworkPagePreview from './preview-templates/NetworkPagePreview'
@@ -14,5 +15,6 @@ CMS.registerMediaLibrary(cloudinary)
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('contact', ContactPagePreview)
-CMS.registerPreviewTemplate('journal', JournalPostPreview)
+CMS.registerPreviewTemplate('journal', JournalPagePreview)
+CMS.registerPreviewTemplate('journal-post', JournalPostPreview)
 CMS.registerPreviewTemplate('network', NetworkPagePreview)
