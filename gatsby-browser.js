ea4962bcb4cab7c1b96a0e74ac5ecb35192711f1
Phil Hess <phess@andculture.com>
2020-03-05 14:37:11 -0500

    Add noscrolling to route changes

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,26 @@
 exports.onRouteUpdate = ({ location, prevLocation }) => {
     document.body.style.overflow = '';
 }
+
+exports.shouldUpdateScroll = ({
+    routerProps: { location },
+    prevRouterProps,
+    getSavedScrollPosition,
+}) => {
+    const { pathname } = location;
+
+    if (prevRouterProps) {
+        const {
+            location: { pathname: oldPathname },
+        } = prevRouterProps
+
+        // list of routes for the scroll-to-top-hook
+        const noScrollRoutes = ['/studio/startup', '/studio/upstarts', '/studio/cofounder', '/experiments/lab', '/experiments/enterprise']
+        // if the new route is part of the list above, don't scroll
+        if (noScrollRoutes.indexOf(pathname) !== -1 && noScrollRoutes.indexOf(oldPathname) !== -1) {
+            return false;
+        }
+
+        return true;
+    }
+}
