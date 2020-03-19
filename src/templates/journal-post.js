d5091176ff16ab2c1b8f246b600af4a41ccb0fd5
Phil Hess <phess@andculture.com>
2020-03-19 11:25:37 -0400

    Add sorting by publish date to ventores page

--- src/templates/journal-post.js
+++ src/templates/journal-post.js
@@ -9,73 +9,73 @@ import { types } from '../types/types'
 import FluidImage from '../components/FluidImage'
 
 export const JournalPostTemplate = ({
-    category,
-    heading,
-    subheading,
-    image,
-    introduction,
-    content,
-    contentComponent,
+  category,
+  heading,
+  subheading,
+  image,
+  introduction,
+  content,
+  contentComponent,
 }) => {
-    const PostContent = contentComponent || Content
+  const PostContent = contentComponent || Content
 
-    return (
-        <section className="section">
-            <div>
-                <div>{category}</div>
-                <h1>
-                    {heading}
-                </h1>
-                <div>{subheading}</div>
-                {
-                    image != null &&
-                    <FluidImage
-                        alt={image.alt || heading}
-                        image={image.src} />
-                }
-                <p>{introduction}</p>
-                <PostContent content={content} />
-            </div>
-        </section>
-    )
+  return (
+    <section className="section">
+      <div>
+        <div>{category}</div>
+        <h1>
+          {heading}
+        </h1>
+        <div>{subheading}</div>
+        {
+          image != null &&
+          <FluidImage
+            alt={image.alt || heading}
+            image={image.src} />
+        }
+        <p>{introduction}</p>
+        <PostContent content={content} />
+      </div>
+    </section>
+  )
 }
 
 JournalPostTemplate.propTypes = {
-    content: PropTypes.node.isRequired,
-    contentComponent: PropTypes.func,
-    title: PropTypes.string,
-    category: PropTypes.string,
-    heading: PropTypes.string,
-    subheading: PropTypes.string,
-    image: types.imageProps,
-    introduction: PropTypes.string,
-    seo: types.seoProps,
+  content: PropTypes.node.isRequired,
+  contentComponent: PropTypes.func,
+  title: PropTypes.string,
+  category: PropTypes.string,
+  heading: PropTypes.string,
+  subheading: PropTypes.string,
+  image: types.imageProps,
+  introduction: PropTypes.string,
+  seo: types.seoProps,
 }
 
 const JournalPost = ({ data }) => {
-    const { markdownRemark: post } = data
+  const { markdownRemark: post } = data
 
-    return (
-        <Layout
-            seo={post.frontmatter.seo}>
-            <JournalPostTemplate
-                title={post.frontmatter.title}
-                category={post.frontmatter.category}
-                heading={post.frontmatter.heading}
-                subheading={post.frontmatter.subheading}
-                image={post.frontmatter.image}
-                introduction={post.frontmatter.introduction}
-                content={post.html}
-                contentComponent={HTMLContent}
-            />
-        </Layout>
-    )
+  return (
+    <Layout
+      seo={post.frontmatter.seo}>
+      <JournalPostTemplate
+        title={post.frontmatter.title}
+        category={post.frontmatter.category}
+        heading={post.frontmatter.heading}
+        subheading={post.frontmatter.subheading}
+        image={post.frontmatter.image}
+        introduction={post.frontmatter.introduction}
+        content={post.html}
+        contentComponent={HTMLContent}
+      />
+    </Layout>
+  )
 }
 
 JournalPost.propTypes = {
-    data: PropTypes.shape({
-        markdownRemark: PropTypes.object,
-    }),
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.object,
+  }),
 }
 
 export default JournalPost
@@ -88,7 +88,6 @@ export const pageQuery = graphql`
       frontmatter {
         title
         category
-        date(formatString: "MMMM DD, YYYY")
         heading
         subheading
         image {
