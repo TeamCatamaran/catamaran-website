497d77795877659279289c0add50ba9fd8eaa416
Abdullah Amir <aamir@andculture.com>
2020-03-20 13:21:02 -0400

    404 syleinging

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import Header from '../components/Header'
+
+
+
 
 const NotFoundPage = () => (
-  <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  <Layout
+    bodyClass="-purple -interior"
+    footerHasShapes={true}
+  >
+
+    < Header collageType='404' height="450" />
+    <div className="c-intro container" >
+      <div className="c-notFound container">
+        <p className="c-notFound__textLand">404</p>
+        <label className="c-notFound__textNoFind">
+          We can't find what you're looking for.
+        </label>
+      </div>
     </div>
+
   </Layout>
 )
 
