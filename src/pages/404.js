462fd0aa128f3bf822c4d94f69661f8dcbf605f3
Phil Hess <phess@andculture.com>
2020-04-15 09:34:21 -0400

    Redirect all pages to homepage

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,50 +1,13 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React, { useEffect } from 'react'
+import { navigate } from 'gatsby';
 
-import Cta from '../components/Cta'
-import Layout from '../components/Layout'
-import Header from '../components/Header'
+const NotFoundPage = () => {
 
-const NotFoundPage = () => (
-    <Layout
-        bodyClass="-purple -interior"
-        footerHasShapes={true}>
-        <div className="c-notFound container">
-            <Header
-                collageType="404"
-                section="404"
-                heading="We can't find what you're looking for." />
-        </div>
+    useEffect(() => {
+        navigate('/');
+    }, []);
 
-        <div className="c-notFound container">
-            <div className="c-notFound__link">
-                <span className="c-label">Studio</span>
-                <p>
-                    Lorem ipsum
-                </p>
-                <Link to="/studio/startup" className="c-button -outline">Learn More</Link>
-            </div>
-            <div className="c-notFound__link">
-                <span className="c-label">Experiments</span>
-                <p>
-                    Lorem ipsum
-                </p>
-                <Link to="/experiments/lab" className="c-button -outline">Learn More</Link>
-            </div>
-            <div className="c-notFound__link">
-                <span className="c-label">Journal</span>
-                <p>
-                    Lorem ipsum
-                </p>
-                <Link to="/blog" className="c-button -outline">Read More</Link>
-            </div>
-        </div>
-        <div className="c-notFound">
-            <Cta
-                content={{ content: "How can we help?", text: "Contact", link: { rel: "", url: "\contact" } }}
-                variant="startup" />
-        </div>
-    </Layout>
-)
+    return null;
+}
 
 export default NotFoundPage
